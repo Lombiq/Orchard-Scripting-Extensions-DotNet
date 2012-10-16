ea1539a62e35231e81adcc263eccac04ae41da16
Benedek Farkas <none@none>
2012-10-16 19:01:30 +0200

    C# service files

---
+++ Services/CSharpRuntime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+using Orchard.Environment.Extensions;
+using OrchardHUN.Scripting.Models;
+using System.Reflection;
+
+namespace OrchardHUN.Scripting.CSharp.Services
+{
+    [OrchardFeature("OrchardHUN.Scripting.CSharp")]
+    public class CSharpRuntime : ICSharpRuntime
+    {
+        public string Engine
+        {
+            get { return "C#"; }
+        }
+
+        public dynamic ExecuteExpression(string expression, Models.ScriptScope scope)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic ExecuteFile(string path, ScriptScope scope)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
