d8e347d915969a8d4d1025613510b63524903bd4
Benedek Farkas <none@none>
2012-10-22 18:14:38 +0200

    C# code execution basics

--- Services/CSharpRuntime.cs
+++ Services/CSharpRuntime.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.CodeDom.Compiler;
-using Microsoft.CSharp;
+﻿using Microsoft.CSharp;
+using Orchard;
 using Orchard.Environment.Extensions;
-using OrchardHUN.Scripting.Models;
-using System.Reflection;
+using Orchard.Localization;
 using OrchardHUN.Scripting.Services;
+using System;
+using System.CodeDom.Compiler;
 
 namespace OrchardHUN.Scripting.CSharp.Services
 {
     [OrchardFeature("OrchardHUN.Scripting.CSharp")]
     public class CSharpRuntime : ICSharpRuntime
     {
+        private readonly IOrchardServices _orchardServices;
+
+        public CSharpRuntime(IOrchardServices orchardServices)
+        {
+            _orchardServices = orchardServices;
+        }
+
         public string Engine
         {
             get { return "C#"; }
@@ -18,7 +25,32 @@ namespace OrchardHUN.Scripting.CSharp.Services
 
         public dynamic ExecuteExpression(string expression, ScriptScope scope)
         {
-            throw new NotImplementedException();
+            string code = @"
+                using System;
+                public class Scripting
+                {
+                    public static void Script()
+                    {" +
+                       expression + @"
+                    }
+                }";
+
+            CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+            CompilerParameters compParameters = new CompilerParameters();
+            CompilerResults result = codeProvider.CompileAssemblyFromSource(compParameters, code);
+
+            if (result.Errors.HasErrors)
+            {
+                _orchardServices.Notifier.Add(Orchard.UI.Notify.NotifyType.Error, new LocalizedString("There is a problem with your script."));
+                return false;
+            }
+            else
+            {
+                object myClass = result.CompiledAssembly.CreateInstance("Scripting");
+                myClass.GetType().GetMethod("Script").Invoke(myClass, new object[] { });
+                _orchardServices.Notifier.Add(Orchard.UI.Notify.NotifyType.Information, new LocalizedString("Your script ran successfully."));
+                return true;
+            }
         }
 
         public dynamic ExecuteFile(string path, ScriptScope scope)
