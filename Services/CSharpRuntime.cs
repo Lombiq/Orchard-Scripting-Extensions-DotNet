0cf543bb31e9a68e8663b5669b2b81da31afa07e
Benedek Farkas <farkas.zsb@gmail.com>
2012-11-08 20:49:56 +0100

    C# and VB engines use common logic

--- Services/CSharpRuntime.cs
+++ Services/CSharpRuntime.cs
@@ -1,52 +1,24 @@
-﻿using Microsoft.CSharp;
-using Orchard.Environment.Extensions;
-using OrchardHUN.Scripting.Exceptions;
+﻿using Orchard.Environment.Extensions;
+using OrchardHUN.Scripting.CSharp.Services;
 using OrchardHUN.Scripting.Services;
-using System.CodeDom.Compiler;
-using System.Text;
 
 namespace OrchardHUN.Scripting.DotNet.Services
 {
     [OrchardFeature("OrchardHUN.Scripting.CSharp")]
     public class CSharpRuntime : IScriptingRuntime
     {
-        private readonly IDotNetScriptEventHandler _eventHandler;
+        private readonly IDotNetRuntime _runtime;
 
         public string Engine { get { return "C#"; } }
 
-        public CSharpRuntime(IDotNetScriptEventHandler eventHandler)
+        public CSharpRuntime(IDotNetRuntime runtime)
         {
-            _eventHandler = eventHandler;
+            _runtime = runtime;
         }
 
         public dynamic ExecuteExpression(string expression, ScriptScope scope)
         {
-            _eventHandler.BeforeCompilation(new BeforeDotNetCompilationContext(scope));
-
-            CompilerParameters parameters = new CompilerParameters() { GenerateInMemory = true };            
-            foreach (var item in scope.Assemblies) parameters.ReferencedAssemblies.Add(item.Location);
-            CompilerResults result = new CSharpCodeProvider().CompileAssemblyFromSource(parameters, expression);
-
-            _eventHandler.AfterCompilation(new AfterDotNetCompilationContext(scope, result));
-
-            if (result.Errors.HasErrors)
-            {
-                var builder = new StringBuilder();
-                foreach (var item in result.Errors) builder.Append(item + "\n");
-                throw new ScriptRuntimeException("The C# code could not be executed.", new System.Exception(builder.ToString()));
-            }
-            else
-            {
-                var entryClass = result.CompiledAssembly.CreateInstance("CSharpScripting");
-
-                _eventHandler.BeforeExecution(new BeforeDotNetExecutionContext(scope));
-
-                var scriptResult = entryClass.GetType().GetMethod("Main").Invoke(entryClass, new object[] { });
-
-                _eventHandler.AfterExecution(new AfterDotNetExecutionContext(scope));
-
-                return scriptResult;
-            }
+            return _runtime.CompileAndRun(Engine, expression, scope);
         }
     }
 }
---
+++ Services/DotNetRuntime.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.CSharp;
+using Microsoft.VisualBasic;
+using Orchard;
+using Orchard.Localization;
+using OrchardHUN.Scripting.DotNet;
+using OrchardHUN.Scripting.Exceptions;
+using OrchardHUN.Scripting.Services;
+using System;
+using System.CodeDom.Compiler;
+using System.Text;
+
+namespace OrchardHUN.Scripting.CSharp.Services
+{
+    public interface IDotNetRuntime : IDependency
+    {
+        dynamic CompileAndRun(string engine, string expression, ScriptScope scope);
+    }
+
+    public class DotNetRuntime : IDotNetRuntime
+    {
+        private readonly IDotNetScriptEventHandler _eventHandler;
+
+        public Localizer T { get; set; }
+
+        public DotNetRuntime(IDotNetScriptEventHandler eventHandler)
+        {
+            _eventHandler = eventHandler;
+
+            T = NullLocalizer.Instance;
+        }
+
+        public dynamic CompileAndRun(string engine, string expression, ScriptScope scope)
+        {
+            _eventHandler.BeforeCompilation(new BeforeDotNetCompilationContext(scope));
+
+            CompilerParameters parameters = new CompilerParameters() { GenerateInMemory = true };
+            foreach (var item in scope.Assemblies) parameters.ReferencedAssemblies.Add(item.Location);
+
+            CompilerResults result;
+            switch (engine)
+            {
+                case "C#":
+                    result = new CSharpCodeProvider().CompileAssemblyFromSource(parameters, expression);
+                    break;
+                case "VB":
+                    result = new VBCodeProvider().CompileAssemblyFromSource(parameters, expression);
+                    break;
+                default:
+                    throw new ArgumentException(T("Undefined .NET scripting engine.").ToString());
+            }
+
+            _eventHandler.AfterCompilation(new AfterDotNetCompilationContext(scope, result));
+
+            if (result.Errors.HasErrors)
+            {
+                var builder = new StringBuilder();
+                foreach (var item in result.Errors) builder.Append(Environment.NewLine + item);
+                throw new ScriptRuntimeException(Environment.NewLine + T("The following compile error(s) need to be fixed:") + builder.ToString());
+            }
+            else
+            {
+                var entryClass = result.CompiledAssembly.CreateInstance("DotNetScripting");
+
+                _eventHandler.BeforeExecution(new BeforeDotNetExecutionContext(scope));
+
+                var scriptResult = entryClass.GetType().GetMethod("Main").Invoke(entryClass, new object[] { });
+
+                _eventHandler.AfterExecution(new AfterDotNetExecutionContext(scope));
+
+                return scriptResult;
+            }
+        }
+    }
+}
