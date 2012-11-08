19391fe8e39ca46ef28ddf64a0afe07ae5f1d51d
Benedek Farkas <farkas.zsb@gmail.com>
2012-11-08 21:35:33 +0100

    Dynamic can be used again

---
+++ Services/DotNetAssemblyLoader.cs
@@ -0,0 +1,18 @@
+﻿using OrchardHUN.Scripting.DotNet;
+
+namespace OrchardHUN.Scripting.CSharp.Services
+{
+    public class DotNetAssemblyLoader : IDotNetScriptEventHandler
+    {
+        public void BeforeCompilation(BeforeDotNetCompilationContext context)
+        {
+            context.Scope.LoadAssembly(typeof(System.Dynamic.CallInfo).Assembly); // System.Core.dll
+        }
+
+        public void AfterCompilation(AfterDotNetCompilationContext context) { }
+
+        public void BeforeExecution(BeforeDotNetExecutionContext context) { }
+
+        public void AfterExecution(AfterDotNetExecutionContext context) { }
+    }
+}
