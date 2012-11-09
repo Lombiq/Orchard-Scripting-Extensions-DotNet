b6b11f9c52ba13fa536ff26d59d917e3b230471a
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2012-11-09 15:28:47 +0100

    Adapting to IEngineDescriptor

--- Services/CSharpRuntime.cs
+++ Services/CSharpRuntime.cs
@@ -7,18 +7,24 @@ namespace OrchardHUN.Scripting.DotNet.Services
     [OrchardFeature("OrchardHUN.Scripting.CSharp")]
     public class CSharpRuntime : IScriptingRuntime
     {
+        private readonly IEngineDescriptor _descriptor = new EngineDescriptor("C#", new Orchard.Localization.LocalizedString("C#"));
         private readonly IDotNetRuntime _runtime;
 
-        public string Engine { get { return "C#"; } }
+        public IEngineDescriptor Descriptor
+        {
+            get { return _descriptor; }
+        }
+
 
         public CSharpRuntime(IDotNetRuntime runtime)
         {
             _runtime = runtime;
         }
 
+
         public dynamic ExecuteExpression(string expression, ScriptScope scope)
         {
-            return _runtime.CompileAndRun(Engine, expression, scope);
+            return _runtime.CompileAndRun(Descriptor.Name, expression, scope);
         }
     }
 }
--- Services/VisualBasicRuntime.cs
+++ Services/VisualBasicRuntime.cs
@@ -7,18 +7,24 @@ namespace OrchardHUN.Scripting.DotNet.Services
     [OrchardFeature("OrchardHUN.Scripting.VisualBasic")]
     public class VisualBasicRuntime : IScriptingRuntime
     {
+        private readonly IEngineDescriptor _descriptor = new EngineDescriptor("VB", new Orchard.Localization.LocalizedString("Visual Basic"));
         private readonly IDotNetRuntime _runtime;
 
-        public string Engine { get { return "VB"; } }
+        public IEngineDescriptor Descriptor
+        {
+            get { return _descriptor; }
+        }
+        
 
         public VisualBasicRuntime(IDotNetRuntime runtime)
         {
             _runtime = runtime;
         }
 
+
         public dynamic ExecuteExpression(string expression, ScriptScope scope)
         {
-            return _runtime.CompileAndRun(Engine, expression, scope);
+            return _runtime.CompileAndRun(Descriptor.Name, expression, scope);
         }
     }
 }
