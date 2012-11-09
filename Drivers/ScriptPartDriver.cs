37bf8eecaf7b363321a12a421dc91827951c58dc
Benedek Farkas <farkas.zsb@gmail.com>
2012-11-09 20:13:10 +0100

    Setting C# syntax highlighting for C# and VB in ACE

---
+++ Drivers/ScriptPartDriver.cs
@@ -0,0 +1,28 @@
+﻿using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using OrchardHUN.Scripting.Models;
+
+namespace OrchardHUN.Scripting.DotNet.Drivers
+{
+    public class ScriptPartDriver : ContentPartDriver<ScriptPart>
+    {
+        protected override string Prefix
+        {
+            get { return "OrchardHUN.Scripting.ScriptPart"; }
+        }
+
+        protected override DriverResult Editor(ScriptPart part, dynamic shapeHelper)
+        {
+            return ContentShape("Parts_ScriptDotNet_Edit",
+                    () => shapeHelper.EditorTemplate(
+                            TemplateName: "Parts.ScriptDotNet",
+                            Model: part,
+                            Prefix: Prefix));
+        }
+
+        protected override DriverResult Editor(ScriptPart part, IUpdateModel updater, dynamic shapeHelper)
+        {
+            return Editor(part, shapeHelper);
+        }
+    }
+}
